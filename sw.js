a6a5a36005b2284aaaf0ffab71bcc7e5ffe9692c
DanielBreiner <danielbreinerd@gmail.com>
2019-08-31 15:40:35 +0200

    small tweaks: responsivity, audits

---
+++ sw.js
@@ -0,0 +1,24 @@
+var CACHE_NAME = 'daniel_breiner_portfolio';
+var urlsToCache = [
+    '/',
+    '/index.html',
+    '/src/css/main.css',
+    '/src/img/me.jpg',
+    '/src/img/icons/devpost.png',
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
